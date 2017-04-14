ffe1fd142267816f6a18939be7950defa4c28169
power721 <power721@163.com>
2017-04-14 18:55:56 +0800

    post movies

--- src/main/java/org/har01d/imovie/MyApp.java
+++ src/main/java/org/har01d/imovie/MyApp.java
@@ -5,20 +5,38 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MyApp {
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
         HttpServer server = vertx.createHttpServer();
+        AtomicInteger idGenerator = new AtomicInteger();
+        Map<String, String> movies = new HashMap<>();
 
         Router router = Router.router(vertx);
 
-        router.route("/movies/:id").handler(routingContext -> {
+        router.get("/movies/:id").handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
             HttpServerRequest request = routingContext.request();
             String id = request.getParam("id");
             response.putHeader("content-type", "text/plain");
+            String movie = movies.get(id);
+            if (movie != null) {
+                response.end(movie);
+            } else {
+                response.end("Page Not Found!");
+            }
+        });
+
+        router.post("/movies").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.putHeader("content-type", "text/plain");
+            Integer id = idGenerator.incrementAndGet();
+            movies.put(id.toString(), "Movie " + id);
             response.end("Movie ID: " + id);
         });
 
