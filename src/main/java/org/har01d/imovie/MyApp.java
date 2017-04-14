cd134d49aad093095a13de7229a25e7a80561872
power721 <power721@163.com>
2017-04-14 18:48:33 +0800

    path parameter

--- src/main/java/org/har01d/imovie/MyApp.java
+++ src/main/java/org/har01d/imovie/MyApp.java
@@ -2,6 +2,7 @@ package org.har01d.imovie;
 
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.ext.web.Router;
 
@@ -12,6 +13,14 @@ public class MyApp {
         HttpServer server = vertx.createHttpServer();
 
         Router router = Router.router(vertx);
+
+        router.route("/movies/:id").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            HttpServerRequest request = routingContext.request();
+            String id = request.getParam("id");
+            response.putHeader("content-type", "text/plain");
+            response.end("Movie ID: " + id);
+        });
 
         router.route().handler(routingContext -> {
             HttpServerResponse response = routingContext.response();
