824b1ba687c2f96e668a774b5ae8ebdafcf3f441
power721 <power721@163.com>
2017-04-16 20:36:12 +0800

    fixBase64

--- imovie-crawler/src/main/java/org/har01d/imovie/util/UrlUtils.java
+++ imovie-crawler/src/main/java/org/har01d/imovie/util/UrlUtils.java
@@ -12,20 +12,20 @@ public final class UrlUtils {
         try {
             if (encodedUrl.startsWith("thunder://")) {
                 String base64 = encodedUrl.substring("thunder://".length(), encodedUrl.length());
-                byte[] bytes = Base64.getDecoder().decode(base64);
+                byte[] bytes = Base64.getDecoder().decode(fixBase64(base64));
                 String url = new String(bytes);
                 return url.substring(2, url.length() - 2);
             }
 
             if (encodedUrl.startsWith("qqdl://")) {
                 String base64 = encodedUrl.substring("qqdl://".length(), encodedUrl.length());
-                byte[] bytes = Base64.getDecoder().decode(base64);
+                byte[] bytes = Base64.getDecoder().decode(fixBase64(base64));
                 return new String(bytes);
             }
 
             if (encodedUrl.startsWith("flashget://")) {
                 String base64 = encodedUrl.substring("flashget://".length(), encodedUrl.length());
-                byte[] bytes = Base64.getDecoder().decode(base64);
+                byte[] bytes = Base64.getDecoder().decode(fixBase64(base64));
                 String url = new String(bytes);
                 return url.substring(10, url.length() - 10);
             }
@@ -36,4 +36,24 @@ public final class UrlUtils {
         return encodedUrl;
     }
 
+    private static String fixBase64(String base64) {
+        if (base64.endsWith("/")) {
+            return base64.substring(0, base64.length() - 1);
+        }
+
+        int index;
+        char[] chars = base64.toCharArray();
+        for (index = 0; index < chars.length; index++) {
+            char c = chars[index];
+            if (!isAlphabetic(c) && c != '+' && c != '/' && c != '=') {
+                return base64.substring(0, index);
+            }
+        }
+
+        return base64;
+    }
+
+    private static boolean isAlphabetic(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
 }
--- imovie-crawler/src/test/java/org/har01d/imovie/util/UrlUtilsTest.java
+++ imovie-crawler/src/test/java/org/har01d/imovie/util/UrlUtilsTest.java
@@ -7,12 +7,6 @@ import org.junit.Test;
 public class UrlUtilsTest {
 
     @Test
-    public void test() throws Exception {
-        String encodedUrl = "thunder://QUFmdHA6Ly9keWdvZDI6ZHlnb2QyQGRhdGExLmR5Z29kLmNvbTo5OTkxL+WbnuW9kkRWRC9b55S15b2x5aSp5aCCd3d3LmR5Z29kLmNvbV3lm57lvZJDRDIucm12Ylpa/";
-        System.out.println(UrlUtils.convertUrl(encodedUrl));
-    }
-
-    @Test
     public void convertThunder() throws Exception {
         String encodedUrl = "thunder://QUFodHRwOi8vdG9vbC5sdS90ZXN0LnppcFpa";
         assertEquals("http://tool.lu/test.zip", UrlUtils.convertUrl(encodedUrl));
@@ -27,6 +21,18 @@ public class UrlUtilsTest {
     @Test
     public void convertFlashGet() throws Exception {
         String encodedUrl = "flashget://W0ZMQVNIR0VUXWh0dHA6Ly90b29sLmx1L3Rlc3QuemlwW0ZMQVNIR0VUXQ==";
+        assertEquals("http://tool.lu/test.zip", UrlUtils.convertUrl(encodedUrl));
+    }
+
+    @Test
+    public void testInvalid() throws Exception {
+        String encodedUrl = "thunder://QUFodHRwOi8vdG9vbC5sdS90ZXN0LnppcFpa&srcid=119&verno=1";
+        assertEquals("http://tool.lu/test.zip", UrlUtils.convertUrl(encodedUrl));
+    }
+
+    @Test
+    public void testEndWithSlash() throws Exception {
+        String encodedUrl = "thunder://QUFodHRwOi8vdG9vbC5sdS90ZXN0LnppcFpa/";
         assertEquals("http://tool.lu/test.zip", UrlUtils.convertUrl(encodedUrl));
     }
 
