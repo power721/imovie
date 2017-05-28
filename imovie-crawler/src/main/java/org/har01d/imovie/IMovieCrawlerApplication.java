28c3e9e1ae71f678054d05b4e17a3dc931d24ab9
power721 <power721@163.com>
2017-05-28 12:42:40 +0800

    update IMovieCrawlerApplication

--- imovie-crawler/src/main/java/org/har01d/imovie/IMovieCrawlerApplication.java
+++ imovie-crawler/src/main/java/org/har01d/imovie/IMovieCrawlerApplication.java
@@ -1,18 +1,17 @@
 package org.har01d.imovie;
 
 import java.util.Arrays;
-import java.util.List;
+import org.apache.http.impl.client.BasicCookieStore;
 import org.har01d.imovie.btt.BttCrawler;
-import org.har01d.imovie.domain.Resource;
-import org.har01d.imovie.domain.ResourceRepository;
 import org.har01d.imovie.douban.DouBanCrawler;
 import org.har01d.imovie.douban.DouBanExplorer;
 import org.har01d.imovie.rs05.Rs05Crawler;
-import org.har01d.imovie.util.UrlUtils;
+import org.har01d.imovie.service.DouBanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
@@ -34,16 +33,22 @@ public class IMovieCrawlerApplication implements CommandLineRunner {
     private DouBanExplorer explorer;
 
     @Autowired
-    private ResourceRepository resourceRepository;
+    private DouBanService douBanService;
 
     public static void main(String[] args) {
         SpringApplication.run(IMovieCrawlerApplication.class, args);
     }
 
+    @Bean
+    public BasicCookieStore cookieStore() {
+        return new BasicCookieStore();
+    }
+
     @Override
     public void run(String... strings) throws Exception {
         if (!Arrays.asList(environment.getActiveProfiles()).contains("test")) {
-            rs05Crawler.crawler();
+            douBanService.tryLogin();
+//            rs05Crawler.crawler();
             new Thread(() -> {
                 try {
                     explorer.crawler();
@@ -52,30 +57,8 @@ public class IMovieCrawlerApplication implements CommandLineRunner {
                 }
             }).start();
 
-            bttCrawler.crawler();
+//            bttCrawler.crawler();
 //            douBanCrawler.crawler();
-        }
-    }
-
-    private void testFindMagnet() {
-        List<Resource> resources = resourceRepository.findByUriStartingWith("magnet");
-        for (Resource resource : resources) {
-            String uri = resource.getUri();
-            String magnet = UrlUtils.findMagnet(uri);
-            if (!uri.equals(magnet)) {
-                System.out.println(uri + " -> " + magnet);
-            }
-        }
-    }
-
-    private void testFindEd2k() {
-        List<Resource> resources = resourceRepository.findByUriStartingWith("ed2k");
-        for (Resource resource : resources) {
-            String uri = resource.getUri();
-            String ed2k = UrlUtils.findED2K(uri);
-            if (!uri.equals(ed2k)) {
-                System.out.println(uri + " -> " + ed2k);
-            }
         }
     }
 
