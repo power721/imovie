2ba7f40d55a037db0749e7b4ad81d3edd1bd70b7
power721 <power721@163.com>
2017-05-30 09:54:16 +0800

    update MovieExcerpt

--- imovie-common/src/main/java/org/har01d/imovie/domain/MovieExcerpt.java
+++ imovie-common/src/main/java/org/har01d/imovie/domain/MovieExcerpt.java
@@ -1,17 +1,25 @@
 package org.har01d.imovie.domain;
 
+import java.util.Date;
 import org.springframework.data.rest.core.config.Projection;
 
 @Projection(name = "list", types = {Movie.class})
 public interface MovieExcerpt {
-
     Integer getId();
 
     String getTitle();
+
+    String getSynopsis();
 
     String getThumb();
 
     String getDbUrl();
 
     String getDbScore();
+
+    String getImdbUrl();
+
+    String getImdbScore();
+
+    Date getCreatedTime();
 }
--- imovie-common/src/main/java/org/har01d/imovie/domain/MovieRepository.java
+++ imovie-common/src/main/java/org/har01d/imovie/domain/MovieRepository.java
@@ -1,12 +1,35 @@
 package org.har01d.imovie.domain;
 
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(excerptProjection = MovieExcerpt.class)
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+
+    @RestResource(exported = false)
     List<Movie> findByNameStartsWith(String name);
+
+    @RestResource(exported = false)
     Movie findFirstByDbUrl(String dbUrl);
+
+    @RestResource(exported = false)
     Movie findFirstByImdbUrl(String imdbUrl);
+
+    @RestResource(path = "by-category", rel = "by-category")
+    Page<Movie> findByCategories_Id(@Param("id") Integer categoryId, Pageable pageable);
+
+    @RestResource(path = "by-language", rel = "by-language")
+    Page<Movie> findByLanguages_Id(@Param("id") Integer languageId, Pageable pageable);
+
+    @RestResource(path = "by-region", rel = "by-region")
+    Page<Movie> findByRegions_Id(@Param("id") Integer regionId, Pageable pageable);
+
+//    @RestResource(path = "by-person", rel = "by-person")
+//    @Query("SELECT * from ")
+//    Page<Movie> findByPerson_Id(@Param("id") Integer personId, Pageable pageable);
 }
