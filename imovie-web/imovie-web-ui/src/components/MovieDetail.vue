797175e7a2001b19cfff9f0c69846fdbc27cb00d
power721 <power721@163.com>
2017-06-02 21:53:36 +0800

    update movie view

--- imovie-web/imovie-web-ui/src/components/MovieDetail.vue
+++ imovie-web/imovie-web-ui/src/components/MovieDetail.vue
@@ -9,74 +9,78 @@
     </div>
     <div v-if="movie" id="movie">
       <h2>{{ movie.title }}</h2>
-      <div>
-        <img :src="movie.thumb">
-      </div>
-      <div class="ui items" v-if="movie._embedded.directors.length">
-        <div class="item">
-          <div class="content">
-            <div class="header">导演</div>
-            <div class="description">{{ movie._embedded.directors | join }}</div>
-          </div>
+      <div class="ui grid">
+        <div class="five wide column">
+          <img id="thumb" :src="movie.thumb">
         </div>
-        <div class="item" v-if="movie._embedded.editors.length">
-          <div class="content">
-            <div class="header">编剧</div>
-            <div class="description">{{ movie._embedded.editors | join }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie._embedded.actors.length">
-          <div class="content">
-            <div class="header">主演</div>
-            <div class="description">{{ movie._embedded.actors | join }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie._embedded.categories.length">
-          <div class="content">
-            <div class="header">类型</div>
-            <div class="description">{{ movie._embedded.categories | join }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie._embedded.regions.length">
-          <div class="content">
-            <div class="header">制片国家/地区</div>
-            <div class="description">{{ movie._embedded.regions | join }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie._embedded.languages.length">
-          <div class="content">
-            <div class="header">语言</div>
-            <div class="description">{{ movie._embedded.languages | join }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie.releaseDate">
-          <div class="content">
-            <div class="header">上映日期</div>
-            <div class="description">{{ movie.releaseDate }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie.runningTime">
-          <div class="content">
-            <div class="header">片长</div>
-            <div class="description">{{ movie.runningTime }}</div>
-          </div>
-        </div>
-        <div class="item" v-if="movie.aliases.length">
-          <div class="content">
-            <div class="header">又名</div>
-            <div class="description">{{ movie.aliases | join }}</div>
-          </div>
-        </div>
-        <div class="item">
-          <div class="content">
-            <div class="header">豆瓣评分</div>
-            <div class="description"><a :href="movie.dbUrl" target="_blank">{{ movie.dbScore || '0.0' }}</a></div>
-          </div>
-        </div>
-        <div class="item" v-if="movie.imdbUrl">
-          <div class="content">
-            <div class="header">IMDb链接</div>
-            <div class="description"><a :href="movie.imdbUrl" target="_blank">{{ movie.imdbUrl | imdb }}</a>
+        <div class="eleven wide column">
+          <div class="ui items" v-if="movie._embedded.directors.length">
+            <div class="item">
+              <div class="content">
+                <div class="header">导演</div>
+                <div class="description">{{ movie._embedded.directors | join }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie._embedded.editors.length">
+              <div class="content">
+                <div class="header">编剧</div>
+                <div class="description">{{ movie._embedded.editors | join }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie._embedded.actors.length">
+              <div class="content">
+                <div class="header">主演</div>
+                <div class="description">{{ movie._embedded.actors | join }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie._embedded.categories.length">
+              <div class="content">
+                <div class="header">类型</div>
+                <div class="description">{{ movie._embedded.categories | join }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie._embedded.regions.length">
+              <div class="content">
+                <div class="header">制片国家/地区</div>
+                <div class="description">{{ movie._embedded.regions | join }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie._embedded.languages.length">
+              <div class="content">
+                <div class="header">语言</div>
+                <div class="description">{{ movie._embedded.languages | join }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie.releaseDate">
+              <div class="content">
+                <div class="header">上映日期</div>
+                <div class="description">{{ movie.releaseDate }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie.runningTime">
+              <div class="content">
+                <div class="header">片长</div>
+                <div class="description">{{ movie.runningTime }}</div>
+              </div>
+            </div>
+            <div class="item" v-if="movie.aliases.length">
+              <div class="content">
+                <div class="header">又名</div>
+                <div class="description">{{ movie.aliases | join }}</div>
+              </div>
+            </div>
+            <div class="item">
+              <div class="content">
+                <div class="header">豆瓣评分</div>
+                <div class="description"><a :href="movie.dbUrl" target="_blank">{{ movie.dbScore || '0.0' }}</a></div>
+              </div>
+            </div>
+            <div class="item" v-if="movie.imdbUrl">
+              <div class="content">
+                <div class="header">IMDb链接</div>
+                <div class="description"><a :href="movie.imdbUrl" target="_blank">{{ movie.imdbUrl | imdb }}</a>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -87,9 +91,7 @@
         <p>{{ movie.synopsis }}</p>
       </div>
 
-      <div class="ui horizontal divider">
-        资源
-      </div>
+      <div class="ui horizontal divider">资源</div>
       <div class="ui relaxed divided list">
         <div class="item" v-for="resource in movie.res">
           <i class="magnet middle aligned icon"></i>
@@ -111,6 +113,9 @@
 <style>
   div.description {
     min-height: 10px;
+  }
+  img.thumb {
+    max-width: 450px;
   }
 </style>
 <script>
