07e4573ffcf0e1970b0b15b936590307773ed7b6
power721 <power721@163.com>
2017-06-04 19:38:38 +0800

    update movie-crawler.sh

--- imovie-crawler/movie-crawler.sh
+++ imovie-crawler/movie-crawler.sh
@@ -3,15 +3,15 @@
 WORK_SPACE=~/git/imovie
 JAR="${WORK_SPACE}/imovie-crawler/target/movie-crawler-0.1.jar"
 if [ ! -f "${JAR}" ]; then
-  echo >&2 "$(date) cannot find ${JAR}"
+  echo >&2 "[$(date)] cannot find ${JAR}"
   exit 1
 fi
 
 if pgrep -f movie-crawler-0.1.jar; then
-  echo >&2 "$(date) crawler is executing"
+  echo >&2 "[$(date)] crawler is executing"
   exit 1
 fi
 
 cd ${WORK_SPACE} || exit 1
-echo "$(date) executing crawler now"
+echo "[$(date)] executing crawler now"
 java -XX:+HeapDumpOnOutOfMemoryError -jar imovie-crawler/target/movie-crawler-0.1.jar >/dev/null 2>&1 &
