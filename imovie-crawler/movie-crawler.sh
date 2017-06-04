52f7fa4063f0edddcb61b2aad84c0670e5291fa8
power721 <power721@163.com>
2017-06-04 16:52:55 +0800

    update movie-crawler.sh

--- imovie-crawler/movie-crawler.sh
+++ imovie-crawler/movie-crawler.sh
@@ -3,12 +3,12 @@
 WORK_SPACE='~/git/imovie'
 JAR="${WORK_SPACE}/imovie-crawler/target/movie-crawler-0.1.jar"
 if [ ! -f "${JAR}" ]; then
-  echo >2 "cannot find ${JAR}"
+  echo >&2 "cannot find ${JAR}"
   exit 1
 fi
 
 if pgrep -f movie-crawler-0.1.jar; then
-  echo >2 "crawler is executing"
+  echo >&2 "crawler is executing"
   exit 1
 fi
 
