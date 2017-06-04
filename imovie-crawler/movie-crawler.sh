f0b93c849db13283951afc50894a48c95e2e568e
power721 <power721@163.com>
2017-06-04 16:31:26 +0800

    add movie-crawler.sh

---
+++ imovie-crawler/movie-crawler.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+WORK_SPACE='/home/administrator/git/imovie'
+JAR="${WORK_SPACE}/imovie-crawler/target/movie-crawler-0.1.jar"
+if [ ! -x "${JAR}" ]; then
+  exit 1
+fi
+
+if pgrep -f movie-crawler-0.1.jar; then
+  exit 1
+fi
+
+cd ${WORK_SPACE} || exit 1
+java -XX:+HeapDumpOnOutOfMemoryError -jar imovie-crawler/target/movie-crawler-0.1.jar 2>&1 >1 &
