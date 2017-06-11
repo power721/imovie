3943ad8ea3e62c41c825775e0638454a382ca782
power721 <power721@163.com>
2017-06-11 16:02:09 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+echo "try to update code"
 git pull || exit 1
+echo "build artifacts"
 mvn clean install -DskipTests=true || exit 1
+echo "copy artifact to /opt/imovies"
 cp imovie-web/target/imovie-web-0.1.jar /opt/imovies || exit 1
+echo "restart service"
 sudo service imovie restart
+sudo service imovie status
