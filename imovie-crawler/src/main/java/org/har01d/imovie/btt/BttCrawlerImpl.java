2fa78745ddd92505e56a0ce8528248fd8f07e166
power721 <power721@163.com>
2017-06-22 22:32:02 +0800

    disable 981

--- imovie-crawler/src/main/java/org/har01d/imovie/btt/BttCrawlerImpl.java
+++ imovie-crawler/src/main/java/org/har01d/imovie/btt/BttCrawlerImpl.java
@@ -60,7 +60,7 @@ public class BttCrawlerImpl implements BttCrawler {
         executorService.scheduleWithFixedDelay(() -> work(951), 0, 60, TimeUnit.MINUTES);
         executorService.scheduleWithFixedDelay(() -> work(1183), 0, 360, TimeUnit.MINUTES);
         executorService.scheduleWithFixedDelay(() -> work(950), 0, 60, TimeUnit.MINUTES);
-        executorService.scheduleWithFixedDelay(() -> work(981), 0, 60, TimeUnit.MINUTES);
+//        executorService.scheduleWithFixedDelay(() -> work(981), 0, 60, TimeUnit.MINUTES);
         executorService.scheduleWithFixedDelay(() -> work(1193), 0, 360, TimeUnit.MINUTES);
     }
 
